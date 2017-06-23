3314a157470974a0dcbad3d9390084e998eaec46
Matthew Humphreys <m.humphs@gmail.com>
2017-06-23 16:00:28 +0100

    Further variable name improvements

--- bgc_normalise.m
+++ bgc_normalise.m
@@ -1,25 +1,25 @@
 function nCq_nAq = bgc_normalise(Aq_Cq)
 %bgc_normalise Normalise Aq and Cq for any process to unit vector case
-% === INPUTS ==============================================================
-% Aq_Cq: change in [Aq Cq] due to biogeochemical process / micromol/kg
-% === OUTPUTS =============================================================
+% === INPUT ===============================================================
+%   Aq_Cq: change in [Aq Cq] due to biogeochemical process / micromol/kg
+% === OUTPUT ==============================================================
 % nAq_nCq: input [Aq Cq] normalised such that sqrt(Aq^2 + Cq^2) = 1
 % =========================================================================
 % Written by Matthew P. Humphreys, last updated 2017-06-23
 % =========================================================================
 
 % Unpack input
-Aq_set = Aq_Cq(1);
-Cq_set = Aq_Cq(2);
+Aq = Aq_Cq(1);
+Cq = Aq_Cq(2);
 
 % Normalise PC
-if Cq_set == 0, nCq = 0;
-else, nCq = (abs(Cq_set)/Cq_set) * sqrt(Cq_set^2/(Cq_set^2+Aq_set^2));
+if Cq == 0, nCq = 0;
+else, nCq = (abs(Cq)/Cq) * sqrt(Cq^2/(Cq^2+Aq^2));
 end %if
 
 % Normalise PA
-if Aq_set == 0, nAq = 0;
-else, nAq = (abs(Aq_set)/Aq_set) * sqrt(Aq_set^2/(Cq_set^2+Aq_set^2));
+if Aq == 0, nAq = 0;
+else, nAq = (abs(Aq)/Aq) * sqrt(Aq^2/(Cq^2+Aq^2));
 end %if
 
 % Generate output
