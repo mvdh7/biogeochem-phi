79f08b49aff6172a869585ed6d1bed72956b1d76
Matthew Humphreys <m.humphs@gmail.com>
2017-06-23 16:08:02 +0100

    Commenting notation

--- bgc_phi.m
+++ bgc_phi.m
@@ -2,7 +2,7 @@ function Phi = bgc_phi(Q,Aq_Cq)
 %bgc_phi Calculate disequilibrium CT from isocapnic quotient and
 %         biogeochemical process vector
 % === INPUTS ==============================================================
-%     Q: isocap slope, calculated from bgc_sliso()       /
+%     Q: isocap slope, calculated from bgc_isocap()       /
 % Aq_Cq: change in [AT CT] due to biogeochemical process / micromol/kg
 % === OUTPUTS =============================================================
 %   Phi: extra change in DIC required to not change pCO2 / micromol/kg
