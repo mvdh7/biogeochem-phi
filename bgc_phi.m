94e3ef5703d745fed1c8890132190c768ba3cfd8
Matthew Humphreys <m.humphs@gmail.com>
2017-06-23 16:04:55 +0100

    Updating notation

--- bgc_phi.m
+++ bgc_phi.m
@@ -1,15 +1,16 @@
-function Phi = bgc_phi(sliso,RA_RC)
-%bgc_phi
+function Phi = bgc_phi(Q,Aq_Cq)
+%bgc_phi Calculate disequilibrium CT from isocapnic quotient and
+%         biogeochemical process vector
 % === INPUTS ==============================================================
-% sliso: isocap slope from bgc_sliso()                    /
-% RA_RC: change in [TA DIC] due to biogeochemical process / micromol/kg
+%     Q: isocap slope, calculated from bgc_sliso()       /
+% Aq_Cq: change in [AT CT] due to biogeochemical process / micromol/kg
 % === OUTPUTS =============================================================
-%   Phi: extra change in DIC required to not change pCO2  / micromol/kg
+%   Phi: extra change in DIC required to not change pCO2 / micromol/kg
 % =========================================================================
-% Written by Matthew P. Humphreys, last updated 2017-05-06
+% Written by Matthew P. Humphreys, last updated 2017-06-23
 % =========================================================================
 
 % Phi
-Phi = RA_RC(2) - RA_RC(1) ./ sliso;
+Phi = Aq_Cq(2) - Aq_Cq(1) ./ Q;
 
 end %function bgc_phi
